6138939c693ce4e8a2ec3d3a86f3832f211d0db1
Ankit Pandey <ankitpandey.ap273@gmail.com>
2023-06-02 18:16:16 +0530

    Adds a new parameter to allow deployment of SAM templates larger than 51200 bytes over s3 bucket.
    Feature to deploy a template larger than 51,200 bytes with the command aws cloudformation deploy.
    
    --s3-bucket: The URI of the Amazon S3 bucket where this command uploads your AWS CloudFormation template. This is required to deploy templates that are larger than 51,200 bytes.
    
    AWS documentation: https://docs.aws.amazon.com/cli/latest/reference/cloudformation/deploy/index.html#deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,4 +78,4 @@ output = text
 region = $AWS_REGION" > ~/.aws/config
 
 aws cloudformation package --template-file $TEMPLATE --output-template-file serverless-output.yaml --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
-aws cloudformation deploy --template-file serverless-output.yaml --stack-name $AWS_STACK_NAME $CAPABILITIES $PARAMETER_OVERRIDES $TAGS
+aws cloudformation deploy --template-file serverless-output.yaml --stack-name $AWS_STACK_NAME --s3-bucket $AWS_DEPLOY_BUCKET $CAPABILITIES $PARAMETER_OVERRIDES $TAGS
